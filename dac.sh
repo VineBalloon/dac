dc932cd74b81afbe82505ca75dea6bd65282c51e
VineBalloon <vincentchen2.033@gmail.com>
2019-02-06 14:44:04 +1100

    Initial Commit

---
+++ dac.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [[ ! $(git rev-parse --is-inside-work-tree) ]]
+then
+    # Command already outputs error message to stderr on fail
+    exit
+fi
+
+FILES=$(git diff --name-only)
+if [[ -z "$FILES" ]]
+then
+    >&2 echo "Error: Working Directory is clean!"
+    exit
+fi
+
+for FILE in $FILES
+do
+    git diff $FILE
+
+    echo -n "Would you like to stage $FILE? [Y/n]: "
+    read YN
+    if [[ -n $YN && ! $YN =~ "^[Yy]" ]]
+    then
+        continue
+    fi
+
+    git add $FILE
+
+    echo -n "Message to commit $FILE: "
+    read MESSAGE
+    git commit -m "$MESSAGE"
+    sleep 1
+done
+
+echo -n "Would you like to push these changes? [Y/n]: "
+read YN
+if [[ -n $YN && ! $YN =~ "^[Yy]" ]]
+then
+    exit
+fi
